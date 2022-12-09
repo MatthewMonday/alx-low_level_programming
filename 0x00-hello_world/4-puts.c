72f0a0af312ce347ecd4e4f315fec77f455da256
MatthewMonday <mandongmathewmonday@gmail.com>
2022-12-09 00:48:02 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like buildind a multilingual puzzle");
+	return (0);
+
+}
