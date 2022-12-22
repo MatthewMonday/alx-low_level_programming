3eff628b295f0deb45b7e64402e4fd2b00310125
MatthewMonday <mandongmathewmonday@gmail.com>
2022-12-22 00:43:09 -0800

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - functino that copies a string
+ * @dest: pointer to a charater that will be charged
+ * @src: pointer to a charater that will be changed
+ * @n: value
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *scr, int n)
+{
+	int i;
+
+	i =0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
